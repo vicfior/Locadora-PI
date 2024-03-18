126dfd6a37664edd89ca9f4c0ab1e81493ce8e73
Victória Fior <104793741+vicfior@users.noreply.github.com>
2024-03-18 13:29:51 -0300

    Create Filme.h
    Arquivo.h sobre os filmes e prazo de empréstimo

---
+++ arquioH/Filme.h
@@ -0,0 +1,22 @@
+#ifndef FILME_H
+#define FILME_H 1
+
+typedef struct filme {
+    char titulo [100];
+	char codfilme [10];
+	char ator_principal[100];
+	char ator_coadjuvante[100];
+	char diretor [100];
+	int ano;
+	char genero[20];
+} Filme;
+
+//funções
+Filme* filme_criar();
+Filme* cadastrar_filme(Filme *f);
+void remover_filme(Filme *f);
+void pesquisar_filme(Filme *f, char* codfilme, char *nome_pesquisado);
+void listar_filme();
+void alterar_filme(Filme *f, char *codfilme, char *novo_titulo, char *novo_ator_principal, char *novo_ator_coadjuvante, char *novo_diretor, int novo_ano, char *novo_genero);
+
+#endif
