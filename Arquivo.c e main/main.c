91a9a37152fae83528cfc8c1fe325fa4d6b183ed
Victória Fior <104793741+vicfior@users.noreply.github.com>
2024-04-22 13:58:20 -0300

    Update main.c

--- Arquivo.c e main/main.c
+++ Arquivo.c e main/main.c
@@ -5,30 +5,57 @@
 #include "Pessoa.h"
 #include "Locacao.h"
 
-int main()
-{   
+void menu_filmes() 
+{
     setlocale(LC_ALL, "pt_BR.UTF-8");
-    //Criar menu geral e menu do cadastro
-    /*MENU GERAL
-    BEM VNDO À LOCADORA DE FILMES
-    1 - ALUGAR
-    2 - FILMES
-    3 - CLIENTES
-    4 - OPERAÇÕES
-    6 - SAIR
-    */
+    char caminho [20] = "Filmes.txt";
+    int fescolha;
+    printf("----------------------------------------------------------------------------------------------------------------\n");
+    printf("                                                  CADASTRO DE FILMES                                            \n");
+    printf("----------------------------------------------------------------------------------------------------------------\n");
 
-   /*MENU CADASTRO FILMES
-   1 - CADASTRAR
-   2 - REMOVER
-   3 - ALTERAR
-   4 - PESQUISAR
-   5 - LISTAR
-   6 - VOLTAR
-   7 - SAIR 
-   */
+    printf("Escolha sua opção: \n");
+    printf("1 - CADASTRAR\n");
+    printf("2 - REMOVER\n");
+    printf("3 - PESQUISAR\n");
+    printf("4 - LISTAR\n");
+    printf("5 - VOLTAR\n");
+    printf("6 - SAIR\n");
+    printf("Opção: ");
+    scanf("%d", &fescolha);
+    limpar_buffer();
 
-    /*MENU CADASTRO CLIENTES
+    switch (fescolha)
+    {
+    case 1:
+        cadastrar_filme(caminho);
+        break;
+    case 2: 
+        remover_filme(caminho);
+        break;
+    case 3: 
+        pesquisar_filme(caminho);
+        break;
+    case 4: 
+        listar_filme(caminho);
+        break;
+    case 5: 
+        //voltar ao menu inicial do sistema
+        break;
+    case 6: 
+        printf("Obrigado por utilizar o sistema ");
+		printf("%s", ADEUS"\n");
+		exit(0);
+        break;
+    default:
+        printf("Opção inválida.\n");
+		return;
+    }
+}
+
+void menu_clientes() 
+{
+  /*MENU CADASTRO CLIENTES
     1 - CADASTRAR
     2 - REMOVER
     3 - ALTERAR
@@ -37,16 +64,59 @@ int main()
     6 - VOLTAR
     7 - SAIR 
     */
-
-    /*MENU OPERAÇÕES - TODAS AS FUNÇÕES DE LOCAÇAÕ
+}
+void menu_operacao() 
+{
+   /*MENU OPERAÇÕES - TODAS AS FUNÇÕES DE LOCAÇAÕ
     1 - ALUGAR FILME
     2 - DEVOLVER FILME
     3 - REMOVER FILME
     ...
     */
+}
+void alugar()
+{ 
 
-   //TESTE DE CHAMADA DE FUNÇÕES
-    char caminho [20] = "Filmes.txt";
-    cadastrar_filme(caminho);
-    return 0;
+}
+
+int main()
+{   
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+    int escolha;
+    printf("----------------------------------------------------------------------------------------------------------------\n");
+    printf("                                                  BEM VINDO À LOCADORA DE FILMES                                \n");
+    printf("----------------------------------------------------------------------------------------------------------------\n");
+
+    printf("Escolha sua opção: \n");
+    printf("1 - ALUGAR\n");
+    printf("2 - FILMES\n");
+    printf("3 - CLIENTES\n");
+    printf("4 - OPERAÇÕES\n");
+    printf("5 - SAIR\n");
+    printf("Opção: ");
+    scanf("%d", &escolha);
+
+    switch (escolha)
+    {
+    case 1:
+        alugar();
+        break;
+    case 2: 
+        menu_filmes();
+        break;
+    case 3: 
+        menu_clientes();
+        break;
+    case 4: 
+        menu_operacao();
+        break;
+    case 5: 
+        printf("Obrigado por utilizar o sistema ");
+		printf("%s", ADEUS);
+		exit(0);
+        break;
+    default:
+        printf("Opção inválida.\n");
+		return 0;
+    } 
 }
