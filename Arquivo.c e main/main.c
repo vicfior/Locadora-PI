b9fa06ede39ad6257c4303005767c2c159bc2a5e
Victória Fior <104793741+vicfior@users.noreply.github.com>
2024-05-16 16:01:05 -0300

    Update main.c

--- Arquivo.c e main/main.c
+++ Arquivo.c e main/main.c
@@ -9,9 +9,9 @@
 int main() {
     setlocale(LC_ALL, "pt_BR.UTF-8");
     int escolha;
-    printf("----------------------------------------------------------------------------------------------------------------\n");
-    printf("                                                  BEM VINDO À LOCADORA DE FILMES                                \n");
-    printf("----------------------------------------------------------------------------------------------------------------\n");
+    printf("☆----------------------------------------------------------------------------------------------------------------------☆\n");
+    printf("☆                                                 ☆ BEM VINDO À LOCADORA DE FILMES ☆                                   ☆\n");
+    printf("☆----------------------------------------------------------------------------------------------------------------------☆\n");
 
     printf("Escolha sua opção: \n");
     printf("1 - ALUGAR\n");
@@ -21,6 +21,7 @@ int main() {
     printf("0 - SAIR\n");
     printf("Opção: ");
     scanf("%d", &escolha);
+    printf ("\n");
 
     switch (escolha)
     {
@@ -28,7 +29,7 @@ int main() {
         //alugar();
         break;
     case 2: 
-        //menu_filmes();
+        menu_filmes();
         break;
     case 3: 
         menu_clientes();
@@ -37,7 +38,7 @@ int main() {
         //menu_operacao();
         break;
     case 0: 
-        printf("Obrigado por utilizar o sistema ");
+        printf(".:*☆ Obrigado por visitar nossa locadora .:*☆ ");
 		printf("%s", ADEUS);
 		exit(0);
         break;
@@ -47,74 +48,135 @@ int main() {
     } 
 }
 
+void menu_filmes() {
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+    Lista_filme *listaf = lista_cria_filme();
+    int opcao = 1;
+    char codigo_filme[20];
+    printf("❁----------------------------------------------------------------------------------------------------------------------❁\n");
+    printf("❁                                                 MENU DE FILMES                                                       ❁\n");
+    printf("❁----------------------------------------------------------------------------------------------------------------------❁\n");
+    while (opcao != 0) {
+        printf("\n❁ -------------------- ❁ Escolha sua opção ❁ ---------------------------- ❁");
+        printf("\n❁                      1 - CADASTRAR FILME                                ❁");
+        printf("\n❁                      2 - LISTAR FILME                                   ❁");
+        printf("\n❁                      3 - REMOVER FILME                                  ❁");
+        printf("\n❁                      4 - PESQUISAR FILME                                ❁");
+        printf("\n❁                      5 - VOLTAR AO MENU PRINCIPAL                       ❁");
+        printf("\n❁                      0 - SAIR                                           ❁");
+        printf("\n❁ -------------------- ❁ Escolha sua opção ❁ ---------------------------- ❁");
+        printf("\nOpção: ");
+        scanf("%d", &opcao);
+        limpar_buffer();
+        switch (opcao) {
+        case 1:
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            printf("❁                                         CADASTRANDO FILMES                                              ❁\n");
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            filme_cadastra(listaf);
+            break;
+        case 2:
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            printf("❁                                         LISTANDO FILMES                                                 ❁\n");
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            listar_filme();
+            break;
+        case 3:
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            printf("❁                                         REMOVENDO FILMES                                                ❁\n");
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            remover_filme(listaf);
+            break;
+        case 4:
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            printf("❁                                         PESQUISANDO FILMES                                              ❁\n");
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            pesquisar_filme(listaf);
+            break;
+        case 5:
+            main();
+            break;
+        case 0:
+            printf("Obrigado por utilizar o sistema ");
+            printf("%s", ADEUS);
+            exit(0);
+        default:
+            printf("Opção inválida.\n");
+            break;
+        }
+
+    }
+}
+
 void menu_clientes() {
-     setlocale(LC_ALL, "pt_BR.UTF-8");
+    setlocale(LC_ALL, "pt_BR.UTF-8");
     Lista *lista = lista_cria();
     int opcao = 1;
     char codigo_cliente[20];
-    printf("✿ ------------------------------------------------------------------------------------------------------- ✿\n");
-    printf("✿                                         BEM VINDO AO MENU DE CLIENTES                                   ✿\n");
-    printf("✿ ------------------------------------------------------------------------------------------------------- ✿\n");
-    while(opcao != 0){
-    printf("\n✿ -------------------- ✿ Escolha sua opção ✿ ---------------------------- ✿");
-    printf("\n✿                      1 - CADASTRAR CLIENTE                              ✿");
-    printf("\n✿                      2 - LISTAR CLIENTE                                 ✿");
-    printf("\n✿                      3 - REMOVER CLIENTE                                ✿");
-    printf("\n✿                      4 - PESQUISAR CLIENTE                              ✿");
-    printf("\n✿                      5 - ALTERAR CLIENTE                                ✿");
-    printf("\n✿                      6 - VOLTAR AO MENU PRINCIPAL                       ✿");
-    printf("\n✿                      0 - SAIR                                           ✿");
-    printf("\n✿ ------------------------------------------------------------------------✿");
+    printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+    printf("❁                                         BEM VINDO AO MENU DE CLIENTES                                   ❁\n");
+    printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+    while(opcao != 0) {
+    printf("\n❁ -------------------- ❁ Escolha sua opção ❁ ----------------------------❁");
+    printf("\n❁                      1 - CADASTRAR CLIENTE                              ❁");
+    printf("\n❁                      2 - LISTAR CLIENTE                                 ❁");
+    printf("\n❁                      3 - REMOVER CLIENTE                                ❁");
+    printf("\n❁                      4 - PESQUISAR CLIENTE                              ❁");
+    printf("\n❁                      5 - ALTERAR CLIENTE                                ❁");
+    printf("\n❁                      6 - VOLTAR AO MENU PRINCIPAL                       ❁");
+    printf("\n❁                      0 - SAIR                                           ❁");
+    printf("\n❁ ------------------------------------------------------------------------❁");
     printf("\nOpção: ");
     scanf("%d", &opcao);
     limpar_buffer();
-switch (opcao)
-    {
-    case 1:
-        printf("✿ ------------------------------------------------------------------------------------------------------- ✿\n");
-        printf("✿                                         CADASTRO DE CLIENTES                                            ✿\n");
-        printf("✿ ------------------------------------------------------------------------------------------------------- ✿\n");
-        pessoa_cadastra(lista);
-        break;
-    case 2:
-        printf("✿ ------------------------------------------------------------------------------------------------------- ✿\n");
-        printf("✿                                         LISTAGEM DE CLIENTES                                            ✿\n");
-        printf("✿ ------------------------------------------------------------------------------------------------------- ✿\n");
-        listar_clientes(lista);
-        break;
-    case 3:
-        printf("✿ ------------------------------------------------------------------------------------------------------- ✿\n");
-        printf("✿                                         REMOÇÃO DE CLIENTES                                             ✿\n");
-        printf("✿ ------------------------------------------------------------------------------------------------------- ✿\n");
-        remover_pessoa(lista);
-        break;
-    case 4: 
-        printf("✿ ------------------------------------------------------------------------------------------------------- ✿\n");
-        printf("✿                                         PESQUISA DE CLIENTES                                            ✿\n");
-        printf("✿ ------------------------------------------------------------------------------------------------------- ✿\n");
-        pesquisar_pessoa(lista);
-        break;
-    case 5:
-        printf("✿ ------------------------------------------------------------------------------------------------------- ✿\n");
-        printf("✿                                         ALTERAÇÃO DE CLIENTES                                           ✿\n");
-        printf("✿ ------------------------------------------------------------------------------------------------------- ✿\n");
-        printf("Digite o código do cliente: ");
-        fgets(codigo_cliente, sizeof(codigo_cliente), stdin);
-        codigo_cliente[strcspn(codigo_cliente, "\n")] = '\0'; 
-        alterar_clientes(codigo_cliente);
-        break;
-    case 6:
-        main();
-        break;
-    case 0: 
-        printf("✿✿ Obrigado por utilizar o sistema ✿✿\n");
-        printf("%s", ADEUS"\n");
-        exit(0);
-        break;
-    default:
-        printf("Opção inválida.\n");
-        break;
-        }
+    switch (opcao)
+        {
+        case 1:
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            printf("❁                                         CADASTRANDO CLIENTES                                            ❁\n");
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            pessoa_cadastra(lista);
+            break;
+        case 2:
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            printf("❁                                         LISTANDO CLIENTES                                               ❁\n");
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            listar_clientes(lista);
+            break;
+        case 3:
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            printf("❁                                         REMOVENDO CLIENTES                                              ❁\n");
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            remover_pessoa(lista);
+            break;
+        case 4: 
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            printf("❁                                         PESQUISANDO CLIENTES                                            ❁\n");
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            pesquisar_pessoa(lista);
+            break;
+        case 5:
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            printf("❁                                         ALTERANDO CLIENTES                                              ❁\n");
+            printf("❁ ------------------------------------------------------------------------------------------------------- ❁\n");
+            printf("Digite o código do cliente: ");
+            fgets(codigo_cliente, sizeof(codigo_cliente), stdin);
+            codigo_cliente[strcspn(codigo_cliente, "\n")] = '\0'; 
+            alterar_clientes(codigo_cliente);
+            break;
+        case 6:
+            main();
+            break;
+        case 0: 
+            printf("❁❁ Obrigado por utilizar o sistema ❁❁\n");
+            printf("%s", ADEUS"\n");
+            exit(0);
+            break;
+        default:
+            printf("Opção inválida.\n");
+            break;
+            }
     
     }
 }
+
