ece16919933b14866b9273444c67c3f9f905b294
Victória Fior <104793741+vicfior@users.noreply.github.com>
2024-05-20 16:46:14 -0300

    Update Locacao.h

--- Cabeçalhos/Locacao.h
+++ Cabeçalhos/Locacao.h
@@ -5,58 +5,34 @@
 #include "Filme.h"
 
 typedef struct locacao {
-    char codlocacao [10];
-    Pessoa* cliente;
-    Filme* filme;
-    Pessoa* funcionario;
-    int data_locacao;
-    int prazo;
-    int data_devolucao;
-}Locacao;
+    char codlocacao[20];
+    char codcliente[20];
+    char codfilme[20];
+    char data_locacao[20];
+    char data_devolucao[20];
+    double valorpago;
+    struct locacao *proximo;
+} Locacao;
 
-typedef struct pagamento {
-	char codpagamento[10];
-    Pessoa* cliente;
-    Filme* filme;
-    Pessoa* funcionario;
-    int data_pagamento;
-    float valor_locacao;
-    float valor_juros;
-    int atraso;
-    float valor_multa;
-} Pagamento;
+typedef struct consulta_h {
+	char *propriedades[6]; 
+	char valor[100];
+} Consulta_h;
 
-typedef struct reserva {
-	char codreserva[10];
-	Filme* filme;
-	Pessoa* cliente;
-	int data_reserva;
-	int prazo;
-} Reserva;
+void limpar_buffer_locacao();
+Locacao *locacao_cria();
+void inserir_locacao(Locacao **inicio, Locacao *nova_locacao);
+void locacao_libera(Locacao *locacao);
+int cliente_existe(char* codcliente);
+int filme_existe(char* codfilme);
+int filme_disponivel(char* codfilme);
+void exibir_locacoes();
+void alugar_filme();
+void pesquisar_locacao();
+double aplicar_multa();
+double receber_pagamento();
+void devolver_filme(char *codlocacao);
+void listar_historico();
+void remover_locacao(char *codlocacao);
 
-typedef struct historico {
-	char codhistorico [10];
-	Filme* filme;
-	Pessoa* cliente;
-	struct Pagamento* pagamento;
-} Historico;
-
-
-
-
-//funções
-Locacao* locacao_criar();
-Locacao* cadastrar_locacao(Locacao *alugar);
-void remover_locacao(Locacao *alugar);
-void pesquisar_locacao(Locacao *alugar, char *codlocacao, char *nome_pesquisado);
-void alterar_locacao(Locacao *alugar, char *codlocacao, int novo_prazo, int nova_data_devolucao);
-//pesquisa reservas feitas com um cliente com base no nome do cliente e o código da reserva
-void pesquisar_reserva(Locacao *alugar, char *codreserva, char *nome_pesquisado);
-void reservar_filme(Reserva *reservar);
-void excluir_reserva(Reserva *reservar);
-//exibe o histórico de locação com base no código do histórico, o nome pesquisado do cliente e o código do cliente
-void exibir_historico(Historico *historico, char *codhistorico, char *nome_pesquisado, char *codcliente);
-
-//aqui a função retorna uma mensagem com o valor a ser pago. Também calcula uma possível multa a ser paga de acordo com o atraso e retorna o valor total.
-void receber_pagamento(Pagamento *pagar, int data_devolucao_atual);
 #endif
