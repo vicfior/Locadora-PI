c3ee31bcc40763ff44d20956c433368b5116a86a
Victória Fior <104793741+vicfior@users.noreply.github.com>
2024-06-10 21:29:07 -0300

    Update Terminal.c

--- Arquivo.c e main/Terminal.c
+++ Arquivo.c e main/Terminal.c
@@ -3,16 +3,12 @@
 #include <conio.h>
 #include <locale.h>
 #include <stdlib.h>
+#include "Terminal.h"
+#include "Filme.h"
+#include "Cliente.h"
+#include "Locacao.h"
 
 
-
-#define SETA_ESQUERDA "\u25B6"
-#define SETA_DIREITA "\u25C0"
-#define SETA_CIMA "\u25B2"
-#define SETA_BAIXO "\u25BC"
-
-
-//Para limpar a tela
 void term_cls() {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     COORD coordScreen = {0, 0};
@@ -27,70 +23,158 @@ void term_cls() {
     SetConsoleCursorPosition(hConsole, coordScreen); // posiciona o cursor
 }
 
-void term_move_xy(int x, int y) {
-    COORD coord;
-    coord.X = x;
-    coord.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord); // posiciona o cursor
-}
-
-void tc_get_cols_rows(int *cols, int *rows) {
-    CONSOLE_SCREEN_BUFFER_INFO csbi;
-    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-    *cols = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-    *rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-}
-
-void tc_hide_cursor() {
-    CONSOLE_CURSOR_INFO cursorInfo;
-    GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
-    cursorInfo.bVisible = FALSE;
-    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
-}
-
-void tc_show_cursor() {
-    CONSOLE_CURSOR_INFO cursorInfo;
-    GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
-    cursorInfo.bVisible = TRUE;
-    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
-}
-
-void draw_menu(int selected) {
-    const char *options[] = {
-        "Option 1", 
-        "Option 2", 
-        "Option 3", 
-        "Option 4", 
-        "Option 5", 
-        "Option 6", 
-        "Option 7", 
-        "Option 8"
-    };
-    int num_options = 8;
+void sair_do_Sistema()
+{
     term_cls();
-    int col, lin;
-    tc_get_cols_rows(&col, &lin);
-    for (int i = 0; i < num_options; ++i) {
-        term_move_xy(col / 2 - 5, lin / 2 - num_options / 2 + i);
-        if (i == selected) {
-            printf("-> %s", options[i]);
-        } else {
-            printf("   %s", options[i]);
-        }
+    printf("Saindo do Sistema...\n");
+    getch(); 
+}
+
+int numeroDeOpcoes(int telaAtual) {
+    switch (telaAtual) {
+        case MENU_PRINCIPAL_TELA:
+            return 4; 
+        case MENU_FILMES_TELA:
+            return 6; 
+        case MENU_CLIENTES_TELA:
+            return 7; 
+        case MENU_LOCACAO_TELA:
+            return 12; 
+        default:
+            return 0;
     }
-    term_move_xy(col / 2 - 10, lin / 2 + num_options);
-    printf("Press ESC to exit");
 }
 
-void draw_confirmation(int selected) {
-    int col, lin;
-    tc_get_cols_rows(&col, &lin);
+void exibirMenuPrincipal(int opcaoSelecionada)
+{
     term_cls();
-    term_move_xy(col / 2 - 10, lin / 2);
-    printf("Você selecionou a opção %d", selected + 1);
-    term_move_xy(col / 2 - 10, lin / 2 + 1);
-    printf("Pressione a tecla da direita ( %s ) para voltar", SETA_DIREITA);
-    term_move_xy(col / 2 - 10, lin / 2 + 2);
-    printf("Press ESC to exit");
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+    const char *opcoesMenuPrincipal[] = {
+        "1 - FILMES",
+        "2 - CLIENTES",
+        "3 - SISTEMA DE LOCAÇÃO",
+        "4 - SAIR"};
+
+    printf("================================= "NEG_PURPLE" "BRILHOS" BEM VINDO À LOCADORA DE FILMES "BRILHOS" "NEG_BRANCO" ==============================\n" NEG_PINK,BRILHOS, ANSI_COLOR_RESET);
+
+    for (int i = 0; i < sizeof(opcoesMenuPrincipal) / sizeof(opcoesMenuPrincipal[0]); i++)
+    {
+        if (i == opcaoSelecionada - 1)
+        {
+            printf(NEG_BRANCO " | " ANSI_COLOR_RESET NEG_PINK "%s", " > ");
+        }
+        else
+        {
+            printf(NEG_BRANCO " | " ANSI_COLOR_RESET);
+        }
+        printf("%s\n", opcoesMenuPrincipal[i]);
+    }
+
+    printf("\n");
+    printf(NEG_BRANCO "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾" ANSI_COLOR_RESET "\n");
+    printf(C_GRAY "Navegue por meio das setas %s %s %s %s\n" ANSI_COLOR_RESET, UP, DOWN, LEFT, RIGHT);
 }
 
+void exibirMenuFilmes(int opcaoSelecionada)
+{
+    term_cls();
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+    Lista_filme *listaf = lista_cria_filme();
+    int opcao = 1;
+    char codigo_filme[20];
+
+    const char *opcoesMenuFilmes[] = {
+        "1 - CADASTRAR FILME",
+        "2 - LISTAR FILME",
+        "3 - REMOVER FILME",
+        "4 - PESQUISAR FILME",
+        "5 - VOLTAR AO MENU PRINCIPAL",
+        "6 - SAIR"};
+
+    printf(" ===================================== "NEG_PURPLE" "BRILHOS" MENU DE FILMES "BRILHOS" "NEG_BRANCO" ====================================== " ANSI_COLOR_RESET "\n");
+    for (int i = 0; i < sizeof(opcoesMenuFilmes) / sizeof(opcoesMenuFilmes[0]); i++)
+    {
+        if (i == opcaoSelecionada - 1)
+        {
+            printf(NEG_BRANCO " | " ANSI_COLOR_RESET NEG_PINK "%s", " > ");
+        }
+        else
+        {
+            printf(NEG_BRANCO " | " ANSI_COLOR_RESET);
+        }
+        printf("%s\n" ANSI_COLOR_RESET, opcoesMenuFilmes[i]);
+    }
+
+    printf("\n");
+    printf(NEG_BRANCO "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾" ANSI_COLOR_RESET "\n");
+    printf(C_GRAY "Navegue por meio das setas %s %s %s %s\n", UP, DOWN, LEFT, RIGHT, ANSI_COLOR_RESET);
+}
+
+void exibirMenuClientes(int opcaoSelecionada)
+{
+    term_cls();
+
+    const char *opcoesMenuClientes[] = {
+        "1 - CADASTRAR CLIENTE",
+        "2 - LISTAR CLIENTE",
+        "3 - PESQUISAR CLIENTE",
+        "4 - ALTERAR CLIENTE",
+        "5 - REMOVER CLIENTE",
+        "6 - VOLTAR AO MENU PRINCIPAL",
+        "7 - SAIR"
+        };
+
+    printf(" ===================================== "NEG_PURPLE" "BRILHOS" MENU DE CLIENTES "BRILHOS" "NEG_BRANCO" ===================================== " ANSI_COLOR_RESET "\n");
+    for (int i = 0; i < sizeof(opcoesMenuClientes) / sizeof(opcoesMenuClientes[0]); i++)
+    {
+        if (i == opcaoSelecionada - 1)
+        {
+            printf(NEG_BRANCO " | " ANSI_COLOR_RESET NEG_PINK " > ");
+        }
+        else
+        {
+            printf(NEG_BRANCO " | " ANSI_COLOR_RESET);
+        }
+        printf("%s\n" ANSI_COLOR_RESET, opcoesMenuClientes[i]);
+    }
+
+    printf("\n");
+    printf( "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾" ANSI_COLOR_RESET "\n");
+    printf(C_GRAY "Navegue por meio das setas %s %s %s %s\n", UP, DOWN, LEFT, RIGHT ANSI_COLOR_RESET);
+}
+
+void exibirMenuLocacao(int opcaoSelecionada)
+{
+    term_cls();
+
+    const char *opcoesMenuLocacao[] = {
+        "1 - ALUGAR FILME",
+        "2 - EXIBIR LOCACOES",
+        "3 - PESQUISAR LOCACOES",
+        "4 - PESQUISAR HISTÓRICO DO CLIENTE",
+        "5 - PESQUISAR RESERVA",
+        "6 - DEVOLVER FILME",
+        "7 - RESERVAR FILME",
+        "8 - REMOVER RESERVA",
+        "9 - EXIBIR RESERVAS",
+        "10 - VOLTAR AO MENU PRINCIPAL",
+        "0 - SAIR"};
+
+    printf(" ===================================== "NEG_PURPLE" "BRILHOS" MENU DE LOCAÇÃO "BRILHOS" "NEG_BRANCO" ==================================== " ANSI_COLOR_RESET "\n");
+    for (int i = 0; i < sizeof(opcoesMenuLocacao) / sizeof(opcoesMenuLocacao[0]); i++)
+    {
+        if (i == opcaoSelecionada - 1)
+        {
+            printf(NEG_BRANCO " | " ANSI_COLOR_RESET NEG_PINK " > ");
+        }
+        else
+        {
+            printf(NEG_BRANCO " | " ANSI_COLOR_RESET);
+        }
+        printf("%s\n" ANSI_COLOR_RESET, opcoesMenuLocacao[i]);
+    }
+
+    printf("\n");
+    printf(NEG_BRANCO "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾" ANSI_COLOR_RESET "\n");
+    printf(C_GRAY "Navegue por meio das setas %s %s %s %s\n", UP, DOWN, LEFT, RIGHT ANSI_COLOR_RESET);
+}
